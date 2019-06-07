62e5c9dff62dce15fe982cf2994a7fe0ffc851c6
tacumai <tacuma.igei@gmail.com>
2019-06-07 10:04:19 +0900

    add zone number

--- src/services/models/field.ts
+++ src/services/models/field.ts
@@ -5,12 +5,29 @@ import Deck from "./cards/deck";
 export default class Field {
   deckZone: Deck;
   exDeckZone: Deck;
-  mainMonsterZone: Monster[];
+  mainMonsterZone: { [key: number]: Monster };
   // TODO: 魔法トラップカードの抽象クラスができたら型を変更
-  spellAndTrapZone: Card[];
+  spellAndTrapZone: { [key: number]: Card };
   fieldSpellZone: Card;
   Graveyard: Card[];
   banishmentZone: Card[];
 
-  constructor() {}
+  constructor(deck: Deck, exDeck: Deck) {
+    this.deckZone = deck;
+    this.exDeckZone = exDeck;
+    this.mainMonsterZone = {
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null
+    };
+    this.spellAndTrapZone = {
+      1: null,
+      2: null,
+      3: null,
+      4: null,
+      5: null
+    };
+  }
 }
