9fde95079b1097f0afeccb828888148fa20ede04
takuma <tacuma.igei@gmail.com>
2019-06-12 09:29:53 +0900

    デッキセットアップの処理を追加 (#20)
    * duel setup function
    
    * add set up function
    
    * add todo

--- src/services/managers/duel.ts
+++ src/services/managers/duel.ts
@@ -1,8 +1,9 @@
 import * as fs from "fs";
-import TurnProcessor from "./turnProcessor";
-
-// JSONファイル
+import * as data from "json!../../../resources/monsterData.json";
+import { MonsterMapper } from "../managers/mappers/monsterMapper";
 import Player from "../models/players/player";
+import Card from "../models/cards/card";
+import Deck from "../models/cards/deck";
 
 export default class Duel {
   firstPlayer: Player;
@@ -14,5 +15,18 @@ export default class Duel {
     this.firstPlayer = firstPlayer;
     this.secondPlayer = secondPlayer;
     this.finalDecision = false;
+    this.setUpDeck();
+  }
+
+  private setUpDeck(): void {
+    // TODO: 後攻プレイヤーのセットアップ処理
+    const cards: Card[] = [];
+
+    data["result"].forEach(card => {
+      cards.push(MonsterMapper.build(card));
+    });
+
+    const deck = new Deck(cards);
+    this.firstPlayer.deck = deck;
   }
 }
