4119d2eb5d25198b32718ee5d535b7cbf0d2b3b5
tacumai <tacuma.igei@gmail.com>
2019-06-12 09:11:15 +0900

    duel setup function

--- src/services/managers/duel.ts
+++ src/services/managers/duel.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import * as data from "json!../../../resources/monsterData.json";
+import { MonsterMapper } from "./services/managers/mappers/monsterMapper";
 import TurnProcessor from "./turnProcessor";
 
 // JSONファイル
@@ -14,5 +16,15 @@ export default class Duel {
     this.firstPlayer = firstPlayer;
     this.secondPlayer = secondPlayer;
     this.finalDecision = false;
+  }
+
+  private setUpDeck(): void {
+    const monsters = [];
+
+    data["result"].forEach(monster => {
+      monsters.push(MonsterMapper.build(monster));
+    });
+
+    this.firstPlayer.deck;
   }
 }
